64ec675068fbcf883c47cb6bc7f42ed05f1e021d
Jakub Chrzanowski <jakub@chrzanowski.info>
2025-05-29 07:33:44 +0200

    Document internal API for resolving IntelliJ Platform classes

--- src/main/kotlin/org/jetbrains/kotlinx/jupyter/intellij/utils/IntelliJPlatformClasses.kt
+++ src/main/kotlin/org/jetbrains/kotlinx/jupyter/intellij/utils/IntelliJPlatformClasses.kt
@@ -1,5 +1,8 @@
 package org.jetbrains.kotlinx.jupyter.intellij.utils
 
+/**
+ * A part of the internal API for resolving classes from the currently run IntelliJ Platform.
+ */
 object IntelliJPlatformClasses {
     val PathManager = getClassForName("com.intellij.openapi.application.PathManager")
 }
